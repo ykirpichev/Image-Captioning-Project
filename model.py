7b6e66a7fa0f6677ef1202326fa890ced1317b09
Yury Kirpichev <ykirpichev@gmail.com>
2018-06-09 00:04:42 +0300

    fix: get rid of double <end> in forward method

--- model.py
+++ model.py
@@ -34,17 +34,19 @@ class DecoderRNN(nn.Module):
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.hidden2word = nn.Linear(hidden_size, vocab_size)
 
-        self.hidden = self.init_hidden()
-    
     def forward(self, features, captions):
         embed = self.embed(captions)
+
         embed = torch.cat((features.unsqueeze(1), embed), 1)
+
         lstm_out, _ = self.lstm(embed)
+        # ignore last output, since last input character is <end>
+        # so, we should get <end>, <end> at the end of output
+        lstm_out = lstm_out[:,:-1,:]
 
         # get the scores for the most likely tag for a word
-        print(lstm_out.shape)
         tag_outputs = self.hidden2word(lstm_out)
-        print(tag_outputs.shape)
+
         tag_scores = F.log_softmax(tag_outputs, dim=1)
 
         return tag_scores
@@ -53,10 +55,12 @@ class DecoderRNN(nn.Module):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
         pass
 
-    def init_hidden(self):
+    def init_hidden(self, num_layers, batch_size, hidden_size):
         ''' At the start of training, we need to initialize a hidden state;
             there will be none because the hidden state is formed based on perviously seen data.
             So, this function defines a hidden state with all zeroes and of a specified size.'''
         # The axes dimensions are (n_layers, batch_size, hidden_size)
-        return (torch.zeros(1, 10, self.hidden_size),
-                torch.zeros(1, 10, self.hidden_size))
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        hidden = (torch.zeros(num_layers, batch_size, hidden_size).to(device),
+                  torch.zeros(num_layers, batch_size, hidden_size).to(device))
+        return  hidden
